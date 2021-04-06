c52e553b81c9399e24b340b313dfe6cc5020ca28
Matias Blanco <sm.blanco@hotmail.com>
2021-04-06 13:00:41 -0300

    fix

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ app.use(bodyparser.json());
 // Conexión a Base de datos
 
 
-const connect = async function () {
+
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.nhghk.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 //const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ncdk5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose.connect(uri,
     { useNewUrlParser: true, useUnifiedTopology: true }
 )
-};
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
 
 // import routes
 const authRoutes = require('./routes/auth');
