8675afd9c1f9aa5d56b3caa89f92da06ab25ab71
Matias Blanco <sm.blanco@hotmail.com>
2021-04-06 12:51:39 -0300

    ..

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 // Conexión a Base de datos
+
+
+const connect = async function () {
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.nhghk.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 //const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ncdk5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 mongoose.connect(uri,
     { useNewUrlParser: true, useUnifiedTopology: true }
 )
-.then(() => console.log('Base de datos conectada'))
-.catch(e => console.log('error db:', e))
+};
 
 // import routes
 const authRoutes = require('./routes/auth');
